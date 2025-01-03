64b721146facab831bf699cee071a9aa63a35cb0
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-03 23:36:15 +0530

    adding a code

---
+++ javascript/js2/app.js
@@ -0,0 +1,12 @@
+console.log("Hello World!");
+console.log("apna college");
+
+let a=10;
+let b=20;
+console.log("sum is :",a+b);
+
+let pencilprice=10;
+let erasorprice=7;
+// let output="the total price is :" + (pencilprice + erasorprice)+ " Rupees.";
+let output=``//back ticks
+console.log(output);
