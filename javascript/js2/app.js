3749ef8341bf3dd44b35939d7817d79b38c857e7
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-04 22:51:51 +0530

    adding a code

--- javascript/js2/app.js
+++ javascript/js2/app.js
@@ -8,5 +8,33 @@ console.log("sum is :",a+b);
 let pencilprice=10;
 let erasorprice=7;
 // let output="the total price is :" + (pencilprice + erasorprice)+ " Rupees.";
-let output=``//back ticks
+let output=`the total price is :${pencilprice + erasorprice} Rupees,`;//back ticks
 console.log(output);
+
+//Operator//
+let m=10;
+let n=5;
+n=m;
+console.log(n);//10
+
+
+//Conditional Statements//
+console.log("before my if statement");
+let age=23;
+if(age>=18){
+    console.log("you can vote");
+    console.log("you can drive");
+}
+console.log("after my if statement");
+
+if(age >20){
+    console.log("you are in your 20s");
+}
+
+let firstname="kalyan"
+if(firstname=="kalyan"){
+    console.log(`Welcome  ${firstname}`);
+}
+
+
+
