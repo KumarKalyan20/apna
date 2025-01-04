36e8f93d8d5775a084e22cfbcefaa75e7f6d44e5
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-04 23:53:14 +0530

    adding a code

---
+++ javascript/js2/practice.js
@@ -0,0 +1,63 @@
+let color="green";
+
+//Traffic Light System
+if(color =="red"){
+    console.log("Stop! Light color red");
+}
+if(color ==="yellow"){
+    console.log("Slow down. Light color yellow");
+}
+if(color ==="green"){
+    console.log("Go. Light color green");
+}
+//Check Vote Eligible
+let age =15;
+if(age >=18){
+    console.log("you can vote");
+}
+else if(age >=18){
+    console.log("you cannot vote");
+}
+else if (age <18){
+    console.log("you cannot vote2");
+}
+
+//marks student
+
+let marks=75;
+ if(marks >=80){
+    console.log("A+");
+ }
+ else if(marks >=60){
+        console.log("A");
+ }
+ else if(marks >=34){
+     console.log("Fail");
+}
+//month conditions
+let month="april";
+
+if(month ==="january"){
+    console.log("winter is here");
+}
+else if (month ==="april"){
+    console.log("summer is there");
+}
+
+// if and else statement example
+
+//Traffic Light System
+let col="blue";
+
+if(col =="red"){
+    console.log("Stop! Light color red");
+}
+else if(col ==="yellow"){
+    console.log("Slow down. Light color yellow");
+}
+else if(col ==="green"){
+    console.log("Go. Light color green");
+}
+ else {
+    console.log("traffic light is broken");
+ }
