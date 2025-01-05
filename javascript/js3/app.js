8bbd11ddbdefd11a0eeed26711d0aa1896c8d77c
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-06 00:36:57 +0530

    adding a code

---
+++ javascript/js3/app.js
@@ -0,0 +1,16 @@
+//string methods
+let str="apna college";
+console.log(str);
+
+
+
+//method chaining
+
+let msg = "  hello everyone   ";
+// let newMsg = msg.trim();
+// console.log("after trim :", newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("after uppercase :", newMsg);
+
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
