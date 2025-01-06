043426963f90d0e5830e214912266832e9057f80
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-06 23:52:14 +0530

    added a js3

--- javascript/js3/app.js
+++ javascript/js3/app.js
@@ -14,3 +14,12 @@ let msg = "  hello everyone   ";
 
 let newMsg = msg.trim().toUpperCase();
 console.log(newMsg);
+
+//Slice
+// let names = "kumar";
+// console.log(names.slice(-2));
+
+let key = "ILoveCoding";
+let fruit ="Mango";
+
+
