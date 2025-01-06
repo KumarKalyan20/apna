16cb683e718000200b715a68617a61f516bf7797
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-07 00:36:55 +0530

    adding a code

---
+++ javascript/js3/practice.js
@@ -0,0 +1,18 @@
+//qs1
+let msg1="help!";
+console.log(msg1.trim().toUpperCase());
+
+//qs2
+
+let nams="ApnaCollege";
+nams.slice(4,9);
+nams.indexOf("na");
+nams.replace("apna","our");
+
+//qs3
+
+nams.slice(4);
+nams.slice(4).replace('l','t');
+let newnams = nams.slice(4).replace('l','t');
+newnams
+newnams.replace('l','t');
