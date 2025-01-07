0f12dff8444855bff51d0a28b340e1d5e0483bd6
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-07 23:52:03 +0530

    added to code;

---
+++ javascript/js3/array.js
@@ -0,0 +1,46 @@
+// let student1 ="jeethu";
+// let student2 ="aman";
+// let student3 ="rajat";
+
+let students = ["jeethu","aman","rajat"];
+
+//creating array
+let marks = [99,85,95,75,62];
+let result = [99,85,95,75,62];
+let namess =["adam","bob","cat"];
+let info =["aman", 25, 6,1];
+
+//empty Array
+let newArr = [];
+[4,7,22,46].length;
+
+//Array Mutable
+let fruts =["litchi","kiwi","orange"];
+fruts[0] = "painapple";
+'painapple'
+fruts
+ ['painapple', 'kiwi', 'orange']
+
+ //Array Methos
+let cars =["audi","bmw","maruthi","xuv"];
+cars.push("toyota");
+cars
+ ['audi', 'bmw', 'maruthi', 'xuv', 'toyota']
+
+cars.pop();
+'toyota'
+cars.pop();
+'xuv'
+cars.("toyota");
+cars.unshift("toyota");
+cars
+ ['toyota', 'audi', 'bmw', 'maruthi']
+cars.shift();
+'toyota'
+cars
+ ['audi', 'bmw', 'maruthi']
+let followers =["a","b","c"];
+let blocked = follers.shift();
+
+followers;
+ ['a', 'b', 'c']
