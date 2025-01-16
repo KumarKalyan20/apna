402de59f4ebffa188d5901838ef877787e158cca
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-16 22:35:31 +0530

    added a code

--- javascript/js4/loops.js
+++ javascript/js4/loops.js
@@ -22,3 +22,52 @@ const guess =prompt("guess my favorite movie");
    //  console.log(i);
    
    //  }
+
+  //  let fruits = ["mango", "apple", "banana","litchi","orange"];
+
+  //  for(let i=0; i<fruits.length; i++) {
+  //   console.log(i, fruits[i]);
+  //  }
+
+   //Nested Loops with Nested Array
+
+  //  let heroes = [
+  //       ["prabhas", "allu arjun", "ntr"]
+  //       ["iranman", "wonder women", "flash"]
+  //  ]
+
+  //  for(let i=0; i<heroes.length; i++) {
+  //    console.log(i, heroes[i], heroes[i].length);
+  //    for(let j=0; j<heroes[i].length; j++) {
+  //        console.log(`j=${j}, ${heroes[i][j]}`);
+  //    }
+  //      }
+
+  let student = [["kalyan",88], ["shiva",99],["jeethu",77]];
+
+  for(let i=0; i<student.length; i++) {
+     console.log(`info of student #${i+1}`);
+    for(let j=0; j<student[i].length; j++) {
+      console.log(student[i][j]);
+    }
+  }
+
+  //for loop
+  let fruits = ["mango", "apple", "banana","litchi","orange"];
+   for(i of fruits) {
+    console.log(i);
+   }
+
+   for(char of "apna") {
+    console.log(char);
+   }
+
+   //Nested for loop
+   
+   let heroes = [["prabhas", "allu arjun", "ntr"], ["iranman", "wonder women"]]
+   
+     for(list of heroes) {
+       for(name of list) {
+         console.log(name);
+       }
+     }
