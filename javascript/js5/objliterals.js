75610470af264662e5fe843b3d3889faf036d4e7
kumar kalyan <kumarkalyan200320@gmail.com>
2025-01-25 23:51:39 +0530

    added a code

---
+++ javascript/js5/objliterals.js
@@ -0,0 +1,60 @@
+const student = {
+    name:"kalyan",
+    age:21,
+    marks:94.4,
+    city:"Delhi"
+};
+
+// const item = {
+//     price: 100.99,
+//     discount:50,
+//     colors: ["red", "pink"]
+// };
+
+const post = {
+    username:"@kalyankumar",
+    content:"This is my #firstPost",
+    likes: 150,
+    reposts:5,
+    tags:["@apnacollege", "@delta"]
+};
+//  const obj = {
+//     1: "a",
+//     2: "b",
+//     3: "c",
+//     null:"d",
+//     undefined:"e"
+//  };
+
+// const classInfo = {
+//    aman: {
+//        grade:"A+",
+//        city:"Delhi"
+//    },
+//    shardha: {
+//        grade:"A",
+//        city:"pune"
+//    },
+//    karan: {
+//        grade:"O",
+//        city:"Mumbai"
+//    }
+// };
+
+const classInfo = [
+   {
+     name:"aman",
+     grade:"A+",
+     city:"Delhi"
+   },
+   {
+     name:"shardha",
+     grade:"A",
+     city:"pune"
+   },
+   {
+     name:"karan",
+     grade:"O",
+     city:"Mumbai"
+   }
+];
