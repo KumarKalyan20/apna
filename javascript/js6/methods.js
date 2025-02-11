e60b9e415e0373eafd7d5816cc856b7ebe484e46
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-11 10:14:46 +0530

    added a code

---
+++ javascript/js6/methods.js
@@ -0,0 +1,31 @@
+//Methods function
+
+// const calculator = {
+//     num: 55,
+//     add: function(a,b) {
+//      return a+b;
+//     },
+ 
+//     sub: function(a,b) {
+//      return a-b;
+//     },
+ 
+//     mul: function(a,b) {
+//      return a*b;
+//     }
+//  };
+
+ //Method (Shorthand)
+
+ const calculator = {
+    add(a,b) {
+        return a + b;
+    },
+    sub(a,b) {
+        return a - b;
+    },
+    mul(a,b) {
+        return a * b;
+    }
+ }
+ 
