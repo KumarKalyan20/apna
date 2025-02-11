4061abcc7367d2b3d50816ccfa17935f5b3683cd
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-11 19:35:47 +0530

    added a code

---
+++ javascript/js7/settimeoutfunc.js
@@ -0,0 +1,19 @@
+console.log("Hi there!");
+
+setTimeout(() => {
+    console.log("Apna College");
+}, 4000);
+
+console.log("Welcome to");
+
+let id = setInterval( () => {
+    console.log("Apna College");
+}, 2000);
+
+console.log(id);
+
+let id2 = setInterval( () => {
+    console.log("Apna College");
+}, 2000);
+
+console.log(id2);
