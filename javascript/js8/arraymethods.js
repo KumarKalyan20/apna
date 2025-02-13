a6f74a716bee57516ef44a5c470d24b763e93764
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-13 18:24:05 +0530

    added a code

---
+++ javascript/js8/arraymethods.js
@@ -0,0 +1,30 @@
+//  let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach(function(el) {
+//     console.log(el);
+// });
+
+
+//  function print(el) {
+//     console.log(el);
+//  }
+
+//  arr.forEach(print);
+
+let arr = [{
+    name:"kalyan",
+    marks:95,
+},
+ {
+    name:"manoj",
+    marks:86,
+},
+ {
+    name:"jeethu",
+    marks:70,
+ },
+];
+
+arr.forEach((student) => {
+    console.log(student);
+});
