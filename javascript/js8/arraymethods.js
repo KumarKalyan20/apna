c22f512c68ac3b7c794431b673a707455443a0ee
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-14 17:33:43 +0530

    added a code

--- javascript/js8/arraymethods.js
+++ javascript/js8/arraymethods.js
@@ -1,17 +1,17 @@
-//  let arr = [1, 2, 3, 4, 5];
+ let arr = [1, 2, 3, 4, 5];
 
-// arr.forEach(function(el) {
-//     console.log(el);
-// });
+arr.forEach(function(el) {
+    console.log(el);
+});
 
 
-//  function print(el) {
-//     console.log(el);
-//  }
+ function print(el) {
+    console.log(el);
+ }
 
-//  arr.forEach(print);
+ arr.forEach(print);
 
-let arr = [{
+let students= [{
     name:"kalyan",
     marks:95,
 },
@@ -25,6 +25,31 @@ let arr = [{
  },
 ];
 
-arr.forEach((student) => {
-    console.log(student);
+let gpa = students.map((el) => {
+    return el.marks / 10;
 });
+
+// arr.forEach((student) => {
+//     console.log(student);
+// });
+
+//Map & Filter
+
+let num = [1, 2, 3, 4];
+
+let double = num.map((el) => {
+    return el*2;
+})
+
+//Filter method
+
+let nums = [1, 2, 3, 4, 7, 8, 2, 9, 10,12, 11];
+
+let ans = nums.filter((el) => {
+    // return  el % 2 == 0; //even -> true, odd ->false
+    return el < 5
+});
+
+// Every & sum
+
+[2, 4, 6].every((el) => el%2 == 0);
