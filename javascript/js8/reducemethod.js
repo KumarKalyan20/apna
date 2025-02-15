a60c9341398ce073e82db05a3f697d53f27052a6
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-15 09:29:26 +0530

    added a code

--- javascript/js8/reducemethod.js
+++ javascript/js8/reducemethod.js
@@ -4,3 +4,25 @@ let finalval = nums.reduce((result, el) =>{
      return result + el;
 });
 console.log(finalval);
+
+//Maximum in Array
+
+let arr = [1, 2, 3, 4, 6,77, 45,8];
+
+// let max =-1;
+// for (let i=0; i < arr.length; i++) {
+//     if(max < arr[i]) {
+//         max = arr[i];
+//     }
+// }
+// console.log(max);
+
+let max = arr.reduce((max, el) => {
+    if(max < el) {
+        return el;
+    } else {
+        return max;
+    }
+});
+
+console.log(max);
