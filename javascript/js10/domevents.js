cfc975ff1bd0f5923574a1dc67c9cd96c0fa0456
kumar kalyan <kumarkalyan200320@gmail.com>
2025-02-22 23:47:50 +0530

    added a code

---
+++ javascript/js10/domevents.js
@@ -0,0 +1,29 @@
+let btns = document.querySelectorAll("button");
+
+for(btn of btns) {
+    // btn.onclick = sayHello;
+    // btn.onclick = sayName;
+
+    // btn.addEventListener("click", sayHello);
+    // btn.addEventListener("click", sayName);
+    btn.addEventListener("dblclick", function() {
+        console.log("you double clicked me!");
+    });
+
+
+    // btn.onmouseenter = function() {
+    //     console.log("you enterted a button");
+    // }
+    // console.dir(btn);
+}
+
+function sayHello() {
+    alert("Hello!");
+}
+
+//Event Listner
+
+function sayName() {
+    alert("Apna College");
+}
+
