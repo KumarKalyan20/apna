bd34fde1340a5ab79a6814f892d57cc93e05f4c3
kumar kalyan <kumarkalyan200320@gmail.com>
2025-03-12 16:28:50 +0530

    added a code

--- React.js/react_2/src/App.jsx
+++ React.js/react_2/src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
-import ProductTab from './ProductTab.jsx'
+import "./App.css";
+import ProductTab from "./ProductTab.jsx";
+// import Title from "./Title.jsx";
+// import MsgBox from "./MsgBox";
 
 function App() {
-
-  return (
+   return ( 
     <>
-    <h1>Block buster</h1>
-    <ProductTab/>
+    {/* <MsgBox userName="kalyan" textColor="yellow" />
+    <MsgBox userName="apnacollege" textColor="blue" />
+    <MsgBox userName="aman" textColor="green" /> */}
+    <h2>Blockbuster Deals | shop Now </h2>
+      <ProductTab/>
     </>
-  )
+   );
 }
 
-export default App
+export default App;
--- React.js/react_2/src/Product.jsx
+++ React.js/react_2/src/Product.jsx
@@ -1,14 +1,21 @@
 import "./Product.css";
+import Price from "./Price";
 
-function Product({ title, price, features }) {
-    let isDiscount = price > 30000;
-    let styles = {backgroundColor : isDiscount ? "pink" : "" };
-   
+function Product({ title, idx }) { 
+    let oldPrices = ["12,495","11,900", "1,599", "599"]; 
+    let newPrices = ["8,999", "9,1999", "899", "278"];
+    let Description = [
+        ["8,000 DPI",  "5 programmable buttons"],
+        ["intutive surfaces", "designed by ipad pro"],
+         ["designed by ipad pro", "intutive surfaces"],
+         ["wireless", "optical orientation"],
+        ];
     return (
-        <div className="Product" style={styles}> 
-            <h3>{title}</h3>
-            <h5>Price : {price}</h5>   
-            {isDiscount && <p>Discount of  5%</p> }
+        <div className="Product"> 
+        <h4>{title}</h4>
+           <p>{Description[idx] [0]}</p>
+           <p>{Description[idx] [1]}</p>
+           <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
 }
