40382e2073be885dca86b961c2115d6040589d77
kumar kalyan <kumarkalyan200320@gmail.com>
2025-03-13 23:11:42 +0530

    added a code

--- React.js/react_3/src/App.jsx
+++ React.js/react_3/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import ProductTab from "./ProductTab.jsx";
 import Button from "./Button";
+import Form from "./Form";
 
 function App() {
    return ( 
     <>
-      <Button/>
+      <Form/>
     </>
    );
 }
