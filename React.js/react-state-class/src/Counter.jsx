6d17aea90f876b29b338fc02242d2208d3c5a12f
kumar kalyan <kumarkalyan200320@gmail.com>
2025-03-14 23:40:54 +0530

    added a code

--- React.js/react-state-class/src/Counter.jsx
+++ React.js/react-state-class/src/Counter.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react"; 
 
+function init() {
+    console.log("init was executed");
+    return Math.random();
+}
+
 export default function Counter() {
-   let [count, setCount] = useState(0);
+   let [count, setCount] = useState(init); //initialization
+   console.log("component was rendered"); 
    let incCount = ()=>{
-     setCount(count + 1);
-     
-   };
+     setCount((currCount) => {
+      return currCount + 1;
+     });
    
+    // setCount(25);
+   };
+    
     return (
         <div>
             <h3>Count = {count}</h3>
