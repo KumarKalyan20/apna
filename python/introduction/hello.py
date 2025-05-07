44c14e7b9d47a3704ab1b292ae720a7a0320bd90
kumar kalyan <kumarkalyan200320@gmail.com>
2025-05-07 23:30:40 +0530

    added a code

---
+++ python/introduction/hello.py
@@ -0,0 +1,33 @@
+print("Hello, World!")
+print("This is My First Program")
+
+# python syntax
+x = 4
+y = "Kalyan Kumar"
+
+# python comments
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+#indentation
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#variables
+x = 5
+y = "Hello world"
+
+print(x)
+print(y)
