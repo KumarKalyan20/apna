f0b9829529afdd2ad9367f655d373ebfbe9fb981
kumar kalyan <kumarkalyan200320@gmail.com>
2025-05-09 23:06:44 +0530

    added  a code

---
+++ python/strings/Boolean/Lists/list.py
@@ -0,0 +1,183 @@
+#List Length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#type()
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#list() Constructor
+thislist = list(("apple", "banana", "cherry")) 
+print(thislist)
+
+#Access Items
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#Negative Indexing
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+#Range of Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
+
+#Range of Negative Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+
+# Change List Items
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+#Change a Range of Item
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#Insert Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#Append Items
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert Items
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#Extend List
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+#Add Any Iterable
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+#Remove List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#Remove Specified Index
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#del keyword remove
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#Clear the List
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+#Loop Lists
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+#List Comprehension
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+#Sort Lists
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+#Sort Descending
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Use the copy() method
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# slice Operator
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+#Join Lists
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
---
+++ python/strings/Boolean/boolean.py
@@ -0,0 +1,41 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+#Python Operators
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
+
+
+print(5 + 4 - 7 + 3)
+
+"""
+Additions and subtractions have the same precedence, and we need to calculate from left to right.
+The calculation above reads:
+5 + 4 = 9
+9 - 7 = 2
+2 + 3 = 5 """
