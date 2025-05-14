58ab0028b23b4f6191cc90723e351eadc8f7b158
kumar kalyan <kumarkalyan200320@gmail.com>
2025-05-14 23:12:06 +0530

    added a code

---
+++ python/Module/mymodule.py
@@ -0,0 +1,27 @@
+#Create a Module
+def greeting(name):
+  print("Hello, " + name)
+
+#Use a Module
+import mymodule
+
+mymodule.greeting("Jonathan")
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+import mymodule
+a = mymodule.person1["age"]
+print(a)
+
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
