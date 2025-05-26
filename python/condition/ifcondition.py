a3a1a5014f4a0f6b9eef0a5d0e079313cd79c8bf
kumar kalyan <kumarkalyan200320@gmail.com>
2025-05-27 00:20:56 +0530

    added a code

--- python/condition/ifcondition.py
+++ python/condition/ifcondition.py
@@ -1,3 +1,15 @@
-d:
- cd apna
-git statusd
+# Add three numbers - Adding two number by user input
+# number1 = float(input("Enter first number:"))
+# number2 = float(input("Enter second number: "))
+# number3 = float(input("Enter third number: "))
+
+# sum = number1 + number2 + number3
+# print("The sum of {0} and {1} and {2} is {3}".  format (number1, number2, number3,sum))
+
+
+num1 = int(input("enter a firt number: "))
+num2 = int(input("enter a secondnumber: "))
+
+maximum = max(num1, num2)
+
+print("The maximum of {0} and {1} is {2}".format(num1, num2, maximum))
